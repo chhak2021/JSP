4d5edfc1b32561d6bb98d70c25ea92137e94d879
chhak0503 <chhak0503@gmail.com>
2022-11-22 17:51:22 +0900

    2022/11/22 수업예제

--- Jboard2/src/main/java/kr/co/jboard2/controller/user/EmailAuthController.java
+++ Jboard2/src/main/java/kr/co/jboard2/controller/user/EmailAuthController.java
@@ -1,20 +1,28 @@
 package kr.co.jboard2.controller.user;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
 
-import javax.servlet.RequestDispatcher;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kr.co.jboard2.dao.UserDAO;
-import kr.co.jboard2.service.user.UserService;
-import kr.co.jboard2.vo.TermsVO;
+import com.google.gson.JsonObject;
 
-@WebServlet("/user/terms.do")
-public class TermsController extends HttpServlet {
+import kr.co.jboard2.service.user.UserService;
+
+@WebServlet("/user/emailAuth.do")
+public class EmailAuthController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private UserService service = UserService.INSTANCE;
@@ -26,14 +34,22 @@ public class TermsController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		TermsVO vo = service.selectTerms();
-		req.setAttribute("vo", vo);
+		String email = req.getParameter("email");
 		
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/user/terms.jsp");
-		dispatcher.forward(req, resp);
+		int[] result = service.sendEmailCode(email);
+		
+		// JSON 출력
+		JsonObject json = new JsonObject();
+		json.addProperty("status", result[0]);
+		json.addProperty("code", result[1]);
+		
+		PrintWriter writer = resp.getWriter();
+		writer.print(json.toString());
+		
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	}
+	
 }
--- Jboard2/src/main/webapp/user/register.jsp
+++ Jboard2/src/main/webapp/user/register.jsp
@@ -1,5 +1,57 @@
 <%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
 <jsp:include page="./_header.jsp"/>
+<script>
+	
+	
+	
+	$(function(){
+		
+		let emailCode = 0;
+		
+		$('#btnEmailAuth').click(function(){
+			
+			let email = $('input[name=email]').val();
+			
+			$.ajax({
+				url: '/Jboard2/user/emailAuth.do',
+				method: 'get',
+				data: {"email":email},
+				dataType: 'json',
+				success: function(data){
+					//console.log(data);
+					if(data.status == 1){
+						// 메일 발송 성공
+						emailCode = data.code;
+						
+						$('.emailResult').text('인증코드를 전송 했습니다. 이메일을 확인 하세요.');
+						$('.auth').show();
+					}else{
+						// 메일 발송 실패
+						$('.emailResult').text('이메일을 실패했습니다. 이메일을 확인 후 다시 하시기 바랍니다.');
+						
+					}
+					
+					
+				}
+			});
+		});
+		
+		
+		// 이메일 인증코드 확인
+		$('#btnEmailConfirm').click(function(){
+			
+			let code = $('input[name=auth]').val();
+			
+			if(code == emailCode){
+				$('.emailResult').text('이메일이 인증 되었습니다.');
+			}
+			
+		});
+		
+		
+	});
+
+</script>
 <main id="user">
     <section class="register">
 
@@ -46,10 +98,11 @@
                     <td>
                         
                         <input type="email" name="email" placeholder="이메일 입력"/>
-                        <button type="button"><img src="../img/chk_auth.gif" alt="인증번호 받기"/></button>
+                        <span class="emailResult"></span>
+                        <button type="button" id="btnEmailAuth"><img src="../img/chk_auth.gif" alt="인증번호 받기"/></button>
                         <div class="auth">
                             <input type="text" name="auth" placeholder="인증번호 입력"/>
-                            <button type="button"><img src="../img/chk_confirm.gif" alt="확인"/></button>
+                            <button type="button" id="btnEmailConfirm"><img src="../img/chk_confirm.gif" alt="확인"/></button>
                         </div>
                     </td>
                 </tr>
