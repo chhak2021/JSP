e26c56805c4ff69bdc55d08f7a38aa19456a9c4c
chhak0503 <chhak0503@gmail.com>
2022-11-28 15:49:52 +0900

    2022/11/25 수업예제

--- Jboard2/src/main/java/kr/co/jboard2/service/ArticleService.java
+++ Jboard2/src/main/java/kr/co/jboard2/service/ArticleService.java
@@ -32,14 +32,18 @@ public enum ArticleService {
 		dao.insertFile(parent, newName, fname);
 	}
 	
-	public int selectCountTotal() {
-		return dao.selectCountTotal();
+	public int selectCountTotal(String search) {
+		return dao.selectCountTotal(search);
 	}
 	
 	public void selectArticle() {}
 	
 	public List<ArticleVO> selectArticles(int limitStart) {
 		return dao.selectArticles(limitStart);
+	}
+	
+	public List<ArticleVO> selectArticlesByKeyword(String keyword, int start) {
+		return dao.selectArticlesByKeyword(keyword, start);
 	}
 	
 	public void updateArticle() {}
